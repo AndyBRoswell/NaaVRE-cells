8654a302f62e023c77b918ed183cf7faaac9086f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-07 20:53:57 +0200

    read-file-lines-r-dev-user-name-at-domain-com update

--- read-file-lines-r-dev-user-name-at-domain-com/task.R
+++ read-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -9,7 +9,6 @@ library(jsonlite)
 option_list = list(
 
 make_option(c("--file_path"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
 
 )
@@ -18,7 +17,6 @@ make_option(c("--id"), action="store", default=NA, type="character", help="my de
 opt = parse_args(OptionParser(option_list=option_list))
 
 file_path <- gsub('"', '', opt$file_path)
-id <- gsub('"', '', opt$id)
 id <- gsub('"', '', opt$id)
 
 
