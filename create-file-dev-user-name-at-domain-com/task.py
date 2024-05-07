5a6235b3da2f174ebfe7cb34ae79c851d3afc72f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-08 00:16:21 +0200

    create-file-dev-user-name-at-domain-com update

--- create-file-dev-user-name-at-domain-com/task.py
+++ create-file-dev-user-name-at-domain-com/task.py
@@ -31,6 +31,7 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
+a = 0.2848811551799526
 
 import json
 filename = "/tmp/file_path_" + id + ".json"
