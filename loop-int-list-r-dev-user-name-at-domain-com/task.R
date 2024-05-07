d4d17d2c054138486c1c16fd0e28158c9eb08c39
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-07 21:04:47 +0200

    loop-int-list-r-dev-user-name-at-domain-com update

--- loop-int-list-r-dev-user-name-at-domain-com/task.R
+++ loop-int-list-r-dev-user-name-at-domain-com/task.R
@@ -9,6 +9,7 @@ library(jsonlite)
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--list_of_ints"), action="store", default=NA, type="character", help="my description")
 
 )
@@ -16,6 +17,7 @@ make_option(c("--list_of_ints"), action="store", default=NA, type="character", h
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
+id <- gsub('"', '', opt$id)
 id <- gsub('"', '', opt$id)
 list_of_ints = fromJSON(opt$list_of_ints)
 
