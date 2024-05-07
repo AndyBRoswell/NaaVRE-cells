ae3d9a8fb8e1c77d754354b073f34d142ab9879f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-07 21:09:22 +0200

    install-and-load-the-climwinb-packages-dev-user-name-at-domain-com update

--- install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
+++ install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
@@ -16,7 +16,6 @@ library(zoo)
 
 option_list = list(
 
-make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
 
 )
@@ -24,7 +23,6 @@ make_option(c("--id"), action="store", default=NA, type="character", help="my de
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
-id <- gsub('"', '', opt$id)
 id <- gsub('"', '', opt$id)
 
 
@@ -55,8 +53,6 @@ rolling_mean_temp <- rollmean(temperature_zoo, k = window_size, fill = 0.0)
 temperature_zoo_str <- toString(temperature_zoo)
 rolling_mean_temp_str <- toString(rolling_mean_temp)
 temperature_data_str <- toString(temperature_data)
-
-a = 0.629607143820119
 
 
 
