65b16848da8634774702fd70de48688bfbc07081
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-08-26 14:43:05 +0200

    give-parameter-p-user creation

---
+++ give-parameter-p-user/task.R
@@ -0,0 +1,35 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+param_p <- 'abc'
+
+
+
