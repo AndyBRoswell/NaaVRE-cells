4326f74c24806bf2b1c2cc4e36ea183349a8288d
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-08-26 14:22:20 +0200

    process-list-user update

--- process-list-user/task.R
+++ process-list-user/task.R
@@ -4,12 +4,21 @@ setwd('/app')
 
 library(optparse)
 library(jsonlite)
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
 
 
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--names"), action="store", default=NA, type="character", help="my description")
+make_option(c("--names"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_p"), action="store", default=NA, type="character", help="my description")
 
 )
 
@@ -19,13 +28,22 @@ opt = parse_args(OptionParser(option_list=option_list))
 id <- gsub('"', '', opt$id)
 names = fromJSON(opt$names)
 
+param_p = opt$param_p
 
 
 
 
+library(stringr)
 for (name in names) {
   print(paste0('Hello, ', name))
 }
+x <- 1
+print(param_p)
+print(stringr::str_interp('p = ${param_p}'))
 
 
 
+# capturing outputs
+file <- file(paste0('/tmp/x_', id, '.json'))
+writeLines(toJSON(x, auto_unbox=TRUE), file)
+close(file)
