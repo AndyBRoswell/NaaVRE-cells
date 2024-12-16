a24aa8aedbf70f44cf8eb9a5db8f1e02751a7072
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-12-17 00:36:54 +0100

    print-241217-1-1-lab-original-ver creation

---
+++ print-241217-1-1-lab-original-ver/task.R
@@ -0,0 +1,84 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_a"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--t"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving param_a")
+var = opt$param_a
+print(var)
+var_len = length(var)
+print(paste("Variable param_a has length", var_len))
+
+param_a = opt$param_a
+print("Retrieving t")
+var = opt$t
+print(var)
+var_len = length(var)
+print(paste("Variable t has length", var_len))
+
+t = opt$t
+
+
+print("Running the cell")
+print(t)
+print(param_a)
