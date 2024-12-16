5f40fa680c0dcdff55e1751fd6a691e8539e3239
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-12-17 00:35:22 +0100

    input-name-list-241217-1-1-lab-original-ver creation

---
+++ input-name-list-241217-1-1-lab-original-ver/task.R
@@ -0,0 +1,83 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+	install.packages("dplyr", repos="http://cran.us.r-project.org")
+}
+library(dplyr)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--w"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving w")
+var = opt$w
+print(var)
+var_len = length(var)
+print(paste("Variable w has length", var_len))
+
+w = opt$w
+
+
+print("Running the cell")
+library(dplyr)
+param_a = '`'
+print(w)
+names <- c('Alice', 'Bob')
+# capturing outputs
+print('Serialization of names')
+file <- file(paste0('/tmp/names_', id, '.json'))
+writeLines(toJSON(names, auto_unbox=TRUE), file)
+close(file)
