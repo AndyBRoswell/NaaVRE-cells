adc598e5b765fdc624fe981d17bb78e0d56846e8
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-12-19 03:19:32 +0100

    demo-elc-arrange-data-241217-1-1-lab-original-ver creation

---
+++ demo-elc-arrange-data-241217-1-1-lab-original-ver/task.R
@@ -0,0 +1,94 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--dat"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving dat")
+var = opt$dat
+print(var)
+var_len = length(var)
+print(paste("Variable dat has length", var_len))
+
+print("------------------------Running var_serialization for dat-----------------------")
+print(opt$dat)
+dat = var_serialization(opt$dat)
+print("---------------------------------------------------------------------------------")
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+index_by_numeric_key <- function(S, k) { return(S[[as.character(k)]]) }
+freq <- dat[['freq']]
+alpha_f_ <- setNames(dat[['alpha_f']], freq)
+alpha_f <- function(freq) { return(index_by_numeric_key(alpha_f_, freq)) }
+L_U_ <- setNames(dat[['L_U']], freq)
+L_U <- function(freq) { return(index_by_numeric_key(L_U_, freq)) }
+T_f_ <- setNames(dat[['T_f']], freq)
+T_f <- function(freq) { return(index_by_numeric_key(T_f_, freq)) }
