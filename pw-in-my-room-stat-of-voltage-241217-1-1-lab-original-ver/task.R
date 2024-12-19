02c68e8dde823c0903208987000d6c4405bb80ec
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-12-19 02:16:48 +0100

    pw-in-my-room-stat-of-voltage-241217-1-1-lab-original-ver creation

---
+++ pw-in-my-room-stat-of-voltage-241217-1-1-lab-original-ver/task.R
@@ -0,0 +1,77 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--V"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving V")
+var = opt$V
+print(var)
+var_len = length(var)
+print(paste("Variable V has length", var_len))
+
+print("------------------------Running var_serialization for V-----------------------")
+print(opt$V)
+V = var_serialization(opt$V)
+print("---------------------------------------------------------------------------------")
+
+
+
+print("Running the cell")
+print('min max: ')
+print(range(V))
+print(paste0('avg: ', mean(V)))
+print(paste0('med: ', median(V)))
+print(paste0('\\sigma^2: ', var(V)))
+print('quantile: ')
+print(quantile(V))
