e53274003692eda224df98700069e40af93ae90b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-06-25 18:23:58 +0200

    value-assignment-c25b14e054d65738-rpt-0-250625-1-1-lab-original update

--- value-assignment-c25b14e054d65738-rpt-0-250625-1-1-lab-original/task.R
+++ value-assignment-c25b14e054d65738-rpt-0-250625-1-1-lab-original/task.R
@@ -1,9 +1,7 @@
 setwd('/app')
-
-# retrieve input parameters
-
 library(optparse)
 library(jsonlite)
+
 if (!requireNamespace("dplyr", quietly = TRUE)) {
 	install.packages("dplyr", repos="http://cran.us.r-project.org")
 }
@@ -18,36 +16,69 @@ if (!requireNamespace("stringr", quietly = TRUE)) {
 library(stringr)
 
 
+print('option_list')
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
-
 )
 
-# set input parameters accordingly
+
 opt = parse_args(OptionParser(option_list=option_list))
 
-id <- gsub('"', '', opt$id)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
 
 
-
-
-
+print("Running the cell")
 w <- 0
 x <- 1
 y <- 2
 z <- 3
 param_p <- 'abc'
-
-
-
 # capturing outputs
+print('Serialization of w')
 file <- file(paste0('/tmp/w_', id, '.json'))
 writeLines(toJSON(w, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of x')
 file <- file(paste0('/tmp/x_', id, '.json'))
 writeLines(toJSON(x, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of y')
 file <- file(paste0('/tmp/y_', id, '.json'))
 writeLines(toJSON(y, auto_unbox=TRUE), file)
 close(file)
