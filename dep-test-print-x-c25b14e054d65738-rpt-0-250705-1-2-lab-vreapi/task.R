079f477a9ebebca6fc821fc854d9e741600c5be1
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-06 01:20:02 +0200

    dep-test-print-x-c25b14e054d65738-rpt-0-250705-1-2-lab-vreapi creation

---
+++ dep-test-print-x-c25b14e054d65738-rpt-0-250705-1-2-lab-vreapi/task.R
@@ -0,0 +1,71 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--x_values"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving x_values")
+var = opt$x_values
+print(var)
+var_len = length(var)
+print(paste("Variable x_values has length", var_len))
+
+print("------------------------Running var_serialization for x_values-----------------------")
+print(opt$x_values)
+x_values = var_serialization(opt$x_values)
+print("---------------------------------------------------------------------------------")
+
+
+
+print("Running the cell")
+print(x_values)
