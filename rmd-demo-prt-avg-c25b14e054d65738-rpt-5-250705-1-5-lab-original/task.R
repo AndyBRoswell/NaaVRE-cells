52394043c153b959ccf4701e85bcfbf282a94f02
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-07 18:19:55 +0200

    rmd-demo-prt-avg-c25b14e054d65738-rpt-5-250705-1-5-lab-original creation

---
+++ rmd-demo-prt-avg-c25b14e054d65738-rpt-5-250705-1-5-lab-original/task.R
@@ -0,0 +1,63 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--average"), action="store", default=NA, type="numeric", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving average")
+var = opt$average
+print(var)
+var_len = length(var)
+print(paste("Variable average has length", var_len))
+
+average = opt$average
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+print(average)
