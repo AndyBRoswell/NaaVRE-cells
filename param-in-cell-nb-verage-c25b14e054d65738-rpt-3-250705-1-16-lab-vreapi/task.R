9081936a86279a2d0df2ec2883941103f172b2ef
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-12 16:31:08 +0200

    param-in-cell-nb-verage-c25b14e054d65738-rpt-3-250705-1-16-lab-vreapi creation

---
+++ param-in-cell-nb-verage-c25b14e054d65738-rpt-3-250705-1-16-lab-vreapi/task.R
@@ -0,0 +1,33 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+
+a = 2
+numbers  = c(a, 4, 6, 8, 10)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/numbers_', id, '.json'))
+writeLines(toJSON(numbers, auto_unbox=TRUE), file)
+close(file)
