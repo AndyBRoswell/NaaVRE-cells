b777d26110627a4fd758ed5860b99a171e675347
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-14 01:18:41 +0200

    exynos-7420-pw-plot-settings-c25b14e054d65738-rpt-5-250705-1-18-lab-original creation

---
+++ exynos-7420-pw-plot-settings-c25b14e054d65738-rpt-5-250705-1-18-lab-original/task.R
@@ -0,0 +1,73 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+param_annotation_off_x <- 75
+color <- list('1 core'="blue", "2 cores"="green", "3 cores"="orange", "4 cores"="red")
+# capturing outputs
+print('Serialization of color')
+file <- file(paste0('/tmp/color_', id, '.json'))
+writeLines(toJSON(color, auto_unbox=TRUE), file)
+close(file)
