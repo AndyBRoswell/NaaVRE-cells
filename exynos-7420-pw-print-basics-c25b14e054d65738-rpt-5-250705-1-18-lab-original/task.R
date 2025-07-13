87a892b3217f51f8278075e1fc4de929fdcd9928
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-14 01:18:56 +0200

    exynos-7420-pw-print-basics-c25b14e054d65738-rpt-5-250705-1-18-lab-original creation

---
+++ exynos-7420-pw-print-basics-c25b14e054d65738-rpt-5-250705-1-18-lab-original/task.R
@@ -0,0 +1,82 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--basic"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving basic")
+var = opt$basic
+print(var)
+var_len = length(var)
+print(paste("Variable basic has length", var_len))
+
+print("------------------------Running var_serialization for basic-----------------------")
+print(opt$basic)
+basic = var_serialization(opt$basic)
+print("---------------------------------------------------------------------------------")
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+print('This doc:')
+print(basic[['this']])
+print('Source:')
+print(basic[['src']])
