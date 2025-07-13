af116898eb26f14e688604890464de30b5d1a90c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2025-07-14 01:24:17 +0200

    exynos-pw-plot-power-consumption-curves-c25b14e054d65738-rpt-8-250705-1-18-lab-original creation

---
+++ exynos-pw-plot-power-consumption-curves-c25b14e054d65738-rpt-8-250705-1-18-lab-original/task.R
@@ -0,0 +1,131 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("foreach", quietly = TRUE)) {
+	install.packages("foreach", repos="http://cran.us.r-project.org")
+}
+library(foreach)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--color"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--dat"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--param_annotation_off_x"), action="store", default=NA, type="integer", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving color")
+var = opt$color
+print(var)
+var_len = length(var)
+print(paste("Variable color has length", var_len))
+
+print("------------------------Running var_serialization for color-----------------------")
+print(opt$color)
+color = var_serialization(opt$color)
+print("---------------------------------------------------------------------------------")
+
+print("Retrieving dat")
+var = opt$dat
+print(var)
+var_len = length(var)
+print(paste("Variable dat has length", var_len))
+
+print("------------------------Running var_serialization for dat-----------------------")
+print(opt$dat)
+dat = var_serialization(opt$dat)
+print("---------------------------------------------------------------------------------")
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving param_annotation_off_x")
+var = opt$param_annotation_off_x
+print(var)
+var_len = length(var)
+print(paste("Variable param_annotation_off_x has length", var_len))
+
+param_annotation_off_x = opt$param_annotation_off_x
+
+
+print("Running the cell")
+library(ggplot2)
+p <- ggplot() +
+  geom_line(aes(x = dat[['A53']][['freq']], y = dat[['A53']][['1 core']]), color = color[['1 core']]) +
+  geom_line(aes(x = dat[['A53']][['freq']], y = dat[['A53']][['2 cores']]), color = color[['2 cores']]) +
+  geom_line(aes(x = dat[['A53']][['freq']], y = dat[['A53']][['3 cores']]), color = color[['3 cores']]) +
+  geom_line(aes(x = dat[['A53']][['freq']], y = dat[['A53']][['4 cores']]), color = color[['4 cores']]) +
+  labs(
+    title = "Exynos 7420 Power Consumption Curves (Cortex-A53)",
+    x = "Frequency (MHz)", y = "Power Consumption (W)",
+    color = "Active Cores"
+  ) +
+  theme_minimal() +
+  annotate("text", x = 1500 + param_annotation_off_x, y = 0.367, label = "1 core", color = color[['1 core']]) +
+  annotate("text", x = 1500 + param_annotation_off_x, y = 0.606, label = "2 cores", color = color[['2 cores']]) +
+  annotate("text", x = 1500 + param_annotation_off_x, y = 0.863, label = "3 cores", color = color[['3 cores']]) +
+  annotate("text", x = 1500 + param_annotation_off_x, y = 1.026, label = "4 cores", color = color[['4 cores']])
+q <- ggplot() +
+  geom_line(aes(x = dat[['A57']][['freq']], y = dat[['A57']][['1 core']]), color = color[['1 core']]) +
+  geom_line(aes(x = dat[['A57']][['freq']], y = dat[['A57']][['2 cores']]), color = color[['2 cores']]) +
+  geom_line(aes(x = dat[['A57']][['freq']], y = dat[['A57']][['3 cores']]), color = color[['3 cores']]) +
+  geom_line(aes(x = dat[['A57']][['freq']], y = dat[['A57']][['4 cores']]), color = color[['4 cores']]) +
+  labs(
+    title = "Exynos 7420 Power Consumption Curves (Cortex-A57)",
+    x = "Frequency (MHz)", y = "Power Consumption (W)",
+    color = "Active Cores"
+  ) +
+  theme_minimal() +
+  annotate("text", x = 2100 + param_annotation_off_x, y = 1.62, label = "1 core", color = color[['1 core']]) +
+  annotate("text", x = 2100 + param_annotation_off_x, y = 2.97, label = "2 cores", color = color[['2 cores']]) +
+  annotate("text", x = 2100 + param_annotation_off_x, y = 4.19, label = "3 cores", color = color[['3 cores']]) +
+  annotate("text", x = 2100 + param_annotation_off_x, y = 5.481, label = "4 cores", color = color[['4 cores']])
+print(p)
+print(q)
