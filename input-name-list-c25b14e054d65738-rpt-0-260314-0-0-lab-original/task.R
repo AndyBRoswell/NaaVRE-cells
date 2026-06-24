369796c17b873c8c22b088267604b76ea2f24146
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-06-24 06:03:07 +0200

    input-name-list-c25b14e054d65738-rpt-0-260314-0-0-lab-original update

--- input-name-list-c25b14e054d65738-rpt-0-260314-0-0-lab-original/task.R
+++ input-name-list-c25b14e054d65738-rpt-0-260314-0-0-lab-original/task.R
@@ -20,7 +20,6 @@ print('option_list')
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
 make_option(c("--w"), action="store", default=NA, type="integer", help="my description")
 )
 
@@ -56,13 +55,6 @@ var_serialization <- function(var){
     )
 }
 
-print("Retrieving id")
-var = opt$id
-print(var)
-var_len = length(var)
-print(paste("Variable id has length", var_len))
-
-id <- gsub("\"", "", opt$id)
 print("Retrieving id")
 var = opt$id
 print(var)
