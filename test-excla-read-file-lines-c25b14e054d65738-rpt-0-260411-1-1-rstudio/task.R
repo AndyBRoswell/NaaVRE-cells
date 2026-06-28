42e0ec355cb4475bf2299246aace930494aca98c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-06-28 16:12:21 +0200

    test-excla-read-file-lines-c25b14e054d65738-rpt-0-260411-1-1-rstudio creation

---
+++ test-excla-read-file-lines-c25b14e054d65738-rpt-0-260411-1-1-rstudio/task.R
@@ -0,0 +1,35 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+
+
+option_list = list(
+
+make_option(c("--file_path"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+file_path <- gsub('"', '', opt$file_path)
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+f <- file(file_path, 'r')
+lines = readLines(f)
+close(f)
+
+
+
+# capturing outputs
+file <- file(paste0('/tmp/lines_', id, '.json'))
+writeLines(toJSON(lines, auto_unbox=TRUE), file)
+close(file)
