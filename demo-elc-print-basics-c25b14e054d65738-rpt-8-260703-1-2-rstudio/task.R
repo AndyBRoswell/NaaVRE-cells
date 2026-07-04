3e283d0462e1f37030859150c0884df56728246b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-04 20:47:14 +0200

    demo-elc-print-basics-c25b14e054d65738-rpt-8-260703-1-2-rstudio creation

---
+++ demo-elc-print-basics-c25b14e054d65738-rpt-8-260703-1-2-rstudio/task.R
@@ -0,0 +1,49 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+
+
+option_list = list(
+
+make_option(c("--basic"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+basic = fromJSON(opt$basic)
+id <- gsub('"', '', opt$id)
+
+
+
+
+
+print(basic)
+
+
+
