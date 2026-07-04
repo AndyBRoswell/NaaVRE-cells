42fbe51b51acb46750c1e3b3e383f26c353eceb6
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-04 03:03:36 +0200

    plot-the-data-c25b14e054d65738-rpt-3-260703-1-2-rstudio creation

---
+++ plot-the-data-c25b14e054d65738-rpt-3-260703-1-2-rstudio/task.R
@@ -0,0 +1,43 @@
+setwd('/app')
+
+# retrieve input parameters
+
+library(optparse)
+library(jsonlite)
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+
+
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--x"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--x_values"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--y"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--y_values"), action="store", default=NA, type="character", help="my description")
+
+)
+
+# set input parameters accordingly
+opt = parse_args(OptionParser(option_list=option_list))
+
+id <- gsub('"', '', opt$id)
+x = fromJSON(opt$x)
+x_values = fromJSON(opt$x_values)
+y = fromJSON(opt$y)
+y_values = fromJSON(opt$y_values)
+
+
+
+
+
+data <- data.frame(x=x_values, y=y_values)
+ggplot(data, aes(x = x, y = y)) +
+  geom_line() +
+  geom_point() + # Optional: add points to the line
+  labs(title = "Line Graph of Y vs X", x = "X Values", y = "Y Values")
+
+
+
