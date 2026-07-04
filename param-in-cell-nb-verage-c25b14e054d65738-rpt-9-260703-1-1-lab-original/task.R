afbfb0245eb8f2a9017272741a6ac196b77711ae
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-05 01:46:10 +0200

    param-in-cell-nb-verage-c25b14e054d65738-rpt-9-260703-1-1-lab-original creation

---
+++ param-in-cell-nb-verage-c25b14e054d65738-rpt-9-260703-1-1-lab-original/task.R
@@ -0,0 +1,62 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+
+a = 2
+numbers  = c(a, 4, 6, 8, 10)
+# capturing outputs
+print('Serialization of numbers')
+file <- file(paste0('/tmp/numbers_', id, '.json'))
+writeLines(toJSON(numbers, auto_unbox=TRUE), file)
+close(file)
