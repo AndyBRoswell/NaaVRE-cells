697218da6d153d1708a500acb123186588cd421e
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-05 02:27:37 +0200

    demo-elc-example-settings-c25b14e054d65738-rpt-1-260703-1-1-lab-original creation

---
+++ demo-elc-example-settings-c25b14e054d65738-rpt-1-260703-1-1-lab-original/task.R
@@ -0,0 +1,92 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+	install.packages("ggplot2", repos="http://cran.us.r-project.org")
+}
+library(ggplot2)
+if (!requireNamespace("readr", quietly = TRUE)) {
+	install.packages("readr", repos="http://cran.us.r-project.org")
+}
+library(readr)
+if (!requireNamespace("scales", quietly = TRUE)) {
+	install.packages("scales", repos="http://cran.us.r-project.org")
+}
+library(scales)
+if (!requireNamespace("stringr", quietly = TRUE)) {
+	install.packages("stringr", repos="http://cran.us.r-project.org")
+}
+library(stringr)
+if (!requireNamespace("tidyr", quietly = TRUE)) {
+	install.packages("tidyr", repos="http://cran.us.r-project.org")
+}
+library(tidyr)
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+param_seed <- 0
+examples <- 10
+min_dB <- 0
+max_dB <- 150
+param_digits <- 1
+# capturing outputs
+print('Serialization of examples')
+file <- file(paste0('/tmp/examples_', id, '.json'))
+writeLines(toJSON(examples, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of min_dB')
+file <- file(paste0('/tmp/min_dB_', id, '.json'))
+writeLines(toJSON(min_dB, auto_unbox=TRUE), file)
+close(file)
+print('Serialization of max_dB')
+file <- file(paste0('/tmp/max_dB_', id, '.json'))
+writeLines(toJSON(max_dB, auto_unbox=TRUE), file)
+close(file)
